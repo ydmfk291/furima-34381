447641e8b5edbef1fbb47cd493e966ec555c5803
ydmfk291 <78467544+ydmfk291@users.noreply.github.com>
2021-03-05 18:28:13 +0900

    Basic認証を導入

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,4 @@
 class ApplicationController < ActionController::Base
-  before_action :authenticate_user!
   before_action :configure_permitted_parameters, if: :devise_controller?
 
   before_action :basic_auth
@@ -9,11 +8,12 @@ class ApplicationController < ActionController::Base
 
   def basic_auth
     authenticate_or_request_with_http_basic do |username, password|
-      username == 'admin' && password == '2222'
+      username == ENV["BASIC_AUTH_USERS"] && password == ENV["BASIC_AUTH_PASSWORDS"]
     end
   end
 
   def configure_permitted_parameters
-    devise_parameter_sanitizer.permit(:sign_up, keys: [:nickname])
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:nickname, :family_name, :first_name, :family_name_kana, :first_name_kana, :birthday])
   end
+
 end
---
+++ app/controllers/items_controller.rb
@@ -0,0 +1,16 @@
+class ItemsController < ApplicationController
+  before_action :authenticate_user!, only: [:new,]
+
+  def index
+  end
+
+  def new
+    current_item.update(item_params)
+  end
+
+  def item_params
+    params.require(:item).permit(:image)
+    redirect_to root_path
+    render :new
+  end
+end
