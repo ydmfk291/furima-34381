84644cb8df88a930e0f110fc67a0857fd9c84bc6
ydmfk291 <78467544+ydmfk291@users.noreply.github.com>
2021-03-09 22:50:53 +0900

    バリデーション設定

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,12 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+         validates :password, format: { with: /\A[a-z0-9]+\z/}
+         validates :name, presence: true
+         validates :last_name, presence: true, format: { with: /\A[ぁ-んァ-ン一-龥]/}
+         validates :first_name, presence: true, format: { with: /\A[ぁ-んァ-ン一-龥]/}
+         validates :last_furigana, presence: true, format: { with: /\A[ァ-ヶー－]+\z/}
+         validates :first_furigana, presence: true, format: { with: /\A[ァ-ヶー－]+\z/}
+         validates :birth_date, presence: true
 end
