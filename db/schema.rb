8f201ac6db222dbcfcc638c80e0b5bdc6e4056aa
ydmfk291 <78467544+ydmfk291@users.noreply.github.com>
2021-03-09 18:36:59 +0900

    Devise導入 viewの表示

--- db/schema.rb
+++ db/schema.rb
@@ -16,6 +16,11 @@ ActiveRecord::Schema.define(version: 2021_03_04_131436) do
     t.string "name", null: false
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
+    t.string "last_name", null: false
+    t.string "first_name", null: false
+    t.string "last_name_kana", null: false
+    t.string "first_name_kana", null: false
+    t.date "birth_date", null: false
     t.string "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
