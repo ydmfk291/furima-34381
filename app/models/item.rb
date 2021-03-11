aa3c610da329de730faa0a4db87b879b8e1d6e83
ydmfk291 <78467544+ydmfk291@users.noreply.github.com>
2021-03-11 16:09:02 +0900

    ActiveHashの導入

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,3 +1,9 @@
 class Item < ApplicationRecord
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :category
+  belongs_to :condition
+  belongs_to :postage
+  belongs_to :prefecture
+  belongs_to :delivery
   has_one_attached :image
 end
