3f193150ee530d7fffc0a2cfa6e7d3bbafb5628b
ydmfk291 <78467544+ydmfk291@users.noreply.github.com>
2021-03-11 12:23:04 +0900

    ユーザー管理機能の実装修正２

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,16 +1,4 @@
 class ItemsController < ApplicationController
-  before_action :authenticate_user!, only: [:new]
 
   def index
   end
-
-  def new
-    current_item.update(item_params)
-  end
-
-  def item_params
-    params.require(:item).permit(:image)
-    redirect_to root_path
-    render :new
-  end
-end
