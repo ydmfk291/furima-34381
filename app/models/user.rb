a5894ad2e171a3fa108c45857b21b94247cf44a0
ydmfk291 <78467544+ydmfk291@users.noreply.github.com>
2021-03-11 12:01:06 +0900

    ユーザー管理機能の実装修正2

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,18 +5,17 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
   with_options presence: true do
-    validates :password, format: { with: /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]+\z/i }
     validates :name
+    with_options format: { with: /\A[ぁ-んァ-ヶ一-龥々]+\z/, message: 'Full-width characters' } do
+      validates :family_name
+      validates :first_name
+    end
+    with_options format: { with: /\A[ァ-ヶー－]+\z/, message: 'Full-width katakana characters' } do
+      validates :family_name_kana
+      validates :first_name_kana
+    end
     validates :birth_date
   end
 
-  with_options presence: true, format: { with: /\A(?:\p{Hiragana}|\p{Katakana}|[ー－]|[一-龠々])+\z/ } do
-    validates :family_name
-    validates :first_name
-  end
-
-  with_options presence: true, format: { with: /\A[\p{katakana}\u{30fc}]+\z/ } do
-    validates :family_name_kana
-    validates :first_name_kana
-  end
+  validates :password, format: { with: /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]+\z/i, message: 'Include both letters and numbers' }
 end
