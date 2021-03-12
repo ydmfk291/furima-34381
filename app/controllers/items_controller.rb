9069305c4165525bd45cddebe2060074154f285f
ydmfk291 <78467544+ydmfk291@users.noreply.github.com>
2021-03-12 20:51:16 +0900

    修正４

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -23,10 +23,7 @@ class ItemsController < ApplicationController
   end
 
   def edit
-    if user_signed_in? && current_user.id == @item.user_id 
-    else
-      redirect_to root_path
-    end
+    redirect_to root_path if current_user.id != @item.user_id 
   end
 
   def update
