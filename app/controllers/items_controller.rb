9e1f6edf90678d481ed56bb25e0dd2509ee4483d
ydmfk291 <78467544+ydmfk291@users.noreply.github.com>
2021-03-12 18:02:21 +0900

    修正２

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -19,6 +19,12 @@ class ItemsController < ApplicationController
     end
   end
 
+  def show
+  end
+
+  def edit
+  end
+
   def update
     @item = Item.find(params[:id])
     if @item.update(item_params)
